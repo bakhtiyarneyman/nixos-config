ab03612eba72b1009552d69f1b3c7e24a33da239
maturana <maturana-git.ntf91@aleeas.com>
2022-01-06 23:30:24 -0400

    git: change username

--- users/maturana/configs/programs/git.nix
+++ users/maturana/configs/programs/git.nix
@@ -1,7 +1,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "maturanei";
+    userName = "maturana";
     userEmail = "maturana-git.ntf91@aleeas.com";
     extraConfig = {
       init.defaultBranch = "main";
