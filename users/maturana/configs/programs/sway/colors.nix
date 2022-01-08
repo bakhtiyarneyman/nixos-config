984c189d06b2c30454603274f1228fe7a1e875eb
maturana <maturana-git.ntf91@aleeas.com>
2022-01-08 01:38:31 -0400

    sway: remove not imported module

--- users/maturana/configs/programs/sway/colors.nix
+++ users/maturana/configs/programs/sway/colors.nix
@@ -6,27 +6,27 @@ with colorscheme.colors; {
     background = "#${base0B}";
     text = "#${base00}";
     indicator = "#${base0E}";
-    childBorder = lib.mkForce "";
+    childBorder = "";
   };
   focusedInactive = {
     border = "#${base02}";
     background = "#${base02}";
     text = "#${base00}";
     indicator = "#${base0E}";
-    childBorder = lib.mkForce "";
+    childBorder = "";
   };
   unfocused = {
     border = "#${base02}";
     background = "#${base02}";
     text = "#${base06}";
     indicator = "#${base0E}";
-    childBorder = lib.mkForce "";
+    childBorder = "";
   };
   urgent = {
     border = "#${base0B}";
     background = "#${base0B}";
     text = "#${base00}";
     indicator = "#${base0E}";
-    childBorder = lib.mkForce "";
+    childBorder = "";
   };
 }
