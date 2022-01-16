a3a877fa9d6cda89150eb63579ac9daec615a70b
imMaturana <maturana-git.ntf91@aleeas.com>
2022-01-16 16:03:37 -0400

    ncmpcpp: add notification when song changes

--- users/maturana/configs/programs/ncmpcpp.nix
+++ users/maturana/configs/programs/ncmpcpp.nix
@@ -4,5 +4,8 @@
   programs.ncmpcpp = {
     enable = config.services.mpd.enable;
     mpdMusicDir = builtins.toPath config.services.mpd.musicDirectory;
+    settings = {
+      execute_on_song_change = "notify-send -t 5000 -a \"Ncmpcpp\" \"Now Playing\" \"$(mpc --format '%title% \\n%artist%' current)\"";
+    };
   };
 }
