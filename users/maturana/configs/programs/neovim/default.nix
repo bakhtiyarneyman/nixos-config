6257bd9ffd396c2744a588979db34fca10e4f4d0
imMaturana <maturana-git.ntf91@aleeas.com>
2022-01-21 15:51:31 -0400

    nixvim: fix merge conflict

--- users/maturana/configs/programs/neovim/default.nix
+++ users/maturana/configs/programs/neovim/default.nix
@@ -10,43 +10,8 @@
       relativenumber = true;
       shiftwidth = 2;
     };
-<<<<<<< HEAD
-    colorschemes.gruvbox.enable = true;
-    plugins = {
-      bufferline.enable = true;
-      lualine.enable = true;
-      nvim-tree = {
-        enable = true;
-        view = {
-          side = "left";
-          autoResize = true;
-        };
-        updateCwd = true;
-        diagnostics.enable = true;
-      };
-
-      goyo.enable = true;
-
-      telescope.enable = true;
-
-      nix.enable = true;
-      zig.enable = true;
-      treesitter = {
-        enable = true;
-        indent = true;
-        ensureInstalled = "all";
-      };
-
-      lsp = {
-        enable = true;
-        servers.pyright.enable = true;
-        servers.rnix-lsp.enable = true;
-        servers.zls.enable = true;
-      };
-=======
     globals = {
       mapleader = " ";
->>>>>>> 9f654c7 (flake: use my nixvim fork)
     };
     maps = import ./maps.nix;
     plugins = import ./plugins.nix;
--- users/maturana/configs/programs/neovim/plugins.nix
+++ users/maturana/configs/programs/neovim/plugins.nix
@@ -3,45 +3,23 @@
   lualine.enable = true;
   goyo.enable = true;
   nvim-tree.enable = true;
-<<<<<<< HEAD
-<<<<<<< HEAD
   telescope = {
     enable = true;
   };
   dashboard = let logos = import ./logos.nix; in {
-<<<<<<< HEAD
     enable = true;
+    executive = "telescope";
     header = logos.kurumi-chan;
   };
   nix.enable = true;
   zig.enable = true;
-=======
-=======
-  telescope = {
-    enable = true;
-  };
->>>>>>> 4b64f76 (nixvim: add telescope plugin)
-  dashboard = {
-=======
->>>>>>> f9750e4 (nixvim: add logos to dashboard)
-    enable = true;
-    header = logos.kurumi-chan;
-  };
-  nix.enable = true;
->>>>>>> 9f654c7 (flake: use my nixvim fork)
   treesitter.enable = true;
   lsp = {
     enable = true;
     servers.pyright.enable = true;
     servers.rnix-lsp.enable = true;
     servers.rust-analyzer.enable = true;
+    servers.zls.enable = true;
   };
-<<<<<<< HEAD
-<<<<<<< HEAD
   neogit.enable = true;
-=======
->>>>>>> 9f654c7 (flake: use my nixvim fork)
-=======
-  neogit.enable = true;
->>>>>>> 1591aea (nixvim: add neogit plugin)
 }
