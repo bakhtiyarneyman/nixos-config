eb92956b817e16e46ed782c9cd6b95a32df22de9
imMaturana <maturana-git.ntf91@aleeas.com>
2022-01-21 15:54:18 -0400

    waybar: remove merged patch

--- users/maturana/configs/programs/waybar/default.nix
+++ users/maturana/configs/programs/waybar/default.nix
@@ -3,14 +3,6 @@
 {
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar.overrideAttrs (old: {
-      patches = [
-        (pkgs.fetchurl {
-          url = "https://github.com/Alexays/Waybar/pull/1215.patch";
-          sha256 = "1mxh94lrxyhj5sir1n5nd8qyzilmbmclshnmp7jvvj9fnd69ms8l";
-        })
-      ];
-    });
     settings = [{
       output = "eDP-1";
       layer = "bottom";
